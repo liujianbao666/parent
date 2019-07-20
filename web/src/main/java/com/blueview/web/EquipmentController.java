0a3c24ef5eeeed9daf1ab5a74ea19e34732efd86
liujianbao <liujianbao_kd@163.com>
2019-07-20 16:04:04 +0800

    中间版本

--- web/src/main/java/com/blueview/web/EquipmentController.java
+++ web/src/main/java/com/blueview/web/EquipmentController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
@@ -41,6 +42,13 @@ public class EquipmentController {
         result.put("recordsFiltered", total);
         result.put("data", equipments.getList());
         return result;
+    }
+
+    @RequestMapping(value = "/selectAll")
+    @ResponseBody
+    public List<Equipment> selectAll(@RequestParam(required = false) Equipment equipment) {
+        List<Equipment> equipments = equipmentService.getEquipmentsSelective(equipment);
+        return equipments;
     }
 
     /**
