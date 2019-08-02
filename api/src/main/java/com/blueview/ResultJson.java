569d5b27114778366c79af0e69ea86ca408299f1
liujianbao <liujianbao_kd@163.com>
2019-08-02 09:19:09 +0800

    删除class

--- api/src/main/java/com/blueview/ResultJson.java
+++ api/src/main/java/com/blueview/ResultJson.java
@@ -20,4 +20,24 @@ public class ResultJson {
      * 返回数据对象
      */
     private Object data;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+    
+    
 }
